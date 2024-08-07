25563d6a0743017ecb32e957b99f6c3a4da2a84d
Fomafix <fomafix@googlemail.com>
2024-08-07 07:21:09 +0000

    Use HookHandlers and implement hook interfaces
    Change-Id: I663dcfa2f6810296fa004875612fc220be711351

--- includes/BlockInactiveHooks.php
+++ includes/BlockInactiveHooks.php
@@ -5,14 +5,17 @@ namespace MediaWiki\Extension\BlockInactive;
 use DatabaseUpdater;
 use User;
 
-class BlockInactiveHooks {
+class BlockInactiveHooks implements
+	\MediaWiki\Hook\UserLoginCompleteHook,
+	\MediaWiki\Installer\Hook\LoadExtensionSchemaUpdatesHook
+{
 
 	/**
-	 * @param User &$user
+	 * @param User $user
 	 * @param string &$injectHtml
 	 * @param bool $direct
 	 */
-	public static function onUserLoginComplete( User &$user, string &$injectHtml, bool $direct ) {
+	public function onUserLoginComplete( $user, &$injectHtml, $direct ) {
 		// On a successful login update the `user_touched` in the database
 		// since it is not being updated by default
 		$user->checkAndSetTouched();
@@ -21,7 +24,7 @@ class BlockInactiveHooks {
 	/**
 	 * @param DatabaseUpdater $updater
 	 */
-	public static function onLoadExtensionSchemaUpdates( DatabaseUpdater $updater ) {
+	public function onLoadExtensionSchemaUpdates( $updater ) {
 		$updater->addExtensionTable(
 			'blockinactive_emails',
 			__DIR__ . '/../sql/add_blockinactive_emails.sql'
