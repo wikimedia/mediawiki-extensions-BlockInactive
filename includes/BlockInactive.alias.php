f8baa8d5c5c3b99dfa8b840a087463f2e196a50c
anterdc99 <xuminruixuan@live.cn>
2025-07-13 16:14:41 +0800

    BlockInactive: Translate special page aliases into Chinese
    Change-Id: Ib32ea29fbebdbe21028f61fd9b7d003b1a81ceca

--- includes/BlockInactive.alias.php
+++ includes/BlockInactive.alias.php
@@ -6,3 +6,23 @@ $specialPageAliases = [];
 $specialPageAliases['en'] = [
 	'BlockInactive' => [ 'BlockInactive' ]
 ];
+
+/** Chinese (中文) */
+$specialPageAliases['zh'] = [
+	'BlockInactive' => [ 'BlockInactive' ]
+];
+
+/** Simplified Chinese (中文（简体）) */
+$specialPageAliases['zh-hans'] = [
+	'BlockInactive' => [ '封禁不活跃用户' ]
+];
+
+/** Traditional Chinese (中文（繁體）) */
+$specialPageAliases['zh-hant'] = [
+	'BlockInactive' => [ '封鎖不活躍使用者' ]
+];
+
+/** Chinese (Hong Kong) (中文（香港）) */
+$specialPageAliases['zh-hk'] = [
+	'BlockInactive' => [ '封鎖不活躍用戶' ]
+];
